87fa9c1b63633a7cf07ca48a34a556d9239aea1b
Volodymyr Butyter <butytervolodia@gmail.com>
2022-12-02 01:25:16 +0200

    hw 8 almost finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
 import throttle from 'lodash.throttle';
+
+
+const formEl = document.querySelector('.feedback-form');
+const emailEL = document.querySelector('.feedback-form input');
+const messageEL = document.querySelector('.feedback-form textarea');
+
+
+const formData = {};
+
+const takeFormData = function (e) {
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    console.log(JSON.stringify(formData))
+}
+const submitFormData = function(){
+    localStorage.clear()
+}
+
+const populateFormData = function (e) {
+    const savedMessage = localStorage.getItem('feedback-form-state')
+
+    if (savedMessage) {
+       emailEL.value = 25
+    }
+}
+formEl.addEventListener('input', takeFormData)
+formEl.addEventListener('submit', submitFormData)
+//    localStorage.getItem("feedback-form-state", JSON.parse(formData);)
